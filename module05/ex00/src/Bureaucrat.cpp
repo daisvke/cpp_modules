616754d991f98f57546f1835ebb049b813196189
daisvke <daisuketanigawa@live.fr>
2022-05-20 03:10:51 +0200

    M05.EX00: OK

---
+++ module05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 23:37:44 by dtanigaw          #+#    #+#             */
+/*   Updated: 2022/05/20 02:59:43 by dtanigaw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): _name(""), _grade(_minGrade)
+{
+	std::cout << "Bureaucrat: Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, size_t grade)
+	: _name(name), _grade(grade)
+{
+	std::cout << "Bureaucrat: Parameterized constructor called" << std::endl;
+	checkGrade();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &obj)
+{
+	std::cout << "Bureaucrat: Copy constructor called" << std::endl;
+	*this = obj;
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &obj)
+{
+	std::cout << "Bureaucrat: Assignment operator called" << std::endl;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat: Destructor called" << std::endl;
+}
+
+std::string	Bureaucrat::getName(void) const
+{
+	return _name;
+}
+
+size_t	Bureaucrat::getGrade(void) const
+{
+	return _grade;
+}
+
+void	Bureaucrat::checkGrade(void) const
+{
+	if (_grade > _minGrade)
+		throw (GradeTooLowException());
+	else if (_grade < _maxGrade)
+		throw (GradeTooHighException());
+}
+
+void	Bureaucrat::incremGrade(void)
+{
+	--_grade;
+	checkGrade();
+}
+
+void	Bureaucrat::decremGrade(void)
+{
+	++_grade;
+	checkGrade();
+}
+
+std::ostream	&operator<<(std::ostream &stream, const Bureaucrat &obj)
+{
+	stream << "Name: " << obj.getName()
+	<< ", bureaucrat grade: " << obj.getGrade()
+	<< std::endl;
+
+	return stream;
+}
