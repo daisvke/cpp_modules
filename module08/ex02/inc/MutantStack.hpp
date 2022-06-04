eb46b4aa716783ed99913e7c9fa83e67c2eaf46c
daisvke <daisuketanigawa@live.fr>
2022-06-04 07:17:52 +0200

    doing M8.EX2

--- module08/ex02/inc/MutantStack.hpp
+++ module08/ex02/inc/MutantStack.hpp
@@ -6,10 +6,25 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 09:26:59 by dtanigaw          #+#    #+#             */
-/*   Updated: 2022/06/03 09:28:36 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2022/06/04 07:17:33 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifdef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
 #include <iostream>
+#include <stack>
 
+template<typename T>
+class	MutantStack: public std::stack<T>
+{
+	public:
 
+	MutantStack<T>() {}
+	MutantStack<T>(const MutantStack<T> &obj) {*this = src;}
+	~MutantStack<T>() {};
+	MutantStack<T>	&operator=(const MutantStack<T> &obj) {
+		return *this;
+	}
+};
